52a12871ca5e4067197dbc1875299e9f788303a7
Nikita <nikitavlaev00@gmail.com>
2020-04-13 01:52:10 +0300

    try with post compile

--- obj_detection_yolo/getModels.sh
+++ obj_detection_yolo/getModels.sh
@@ -1,5 +1,4 @@
-cd obj_detection_yolo
-wget https://pjreddie.com/media/files/yolov3.weights -P /obj_detection_yolo/
-wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./yolov3.cfg -P /obj_detection_yolo/
-wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names -P /obj_detection_yolo/
+wget https://pjreddie.com/media/files/yolov3.weights -P obj_detection_yolo/
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O ./yolov3.cfg -P obj_detection_yolo/
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O ./coco.names -P obj_detection_yolo/
 
